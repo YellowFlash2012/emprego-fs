c0f68ef1e3e5ad9c934361085d67d44278902490
YellowFlash2012 <cyclop9@hotmail.com>
2022-06-27 14:43:21 +0100

    customized the errorHandler to take into account the following scenarios:missing fields, unique fields, empty values in controllers. Lastly we updated the signup route to make use of the custome error functions

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,26 @@
 import express from "express"
 import {StatusCodes} from "http-status-codes"
+import { BadRequestError } from "../errors/errors.js";
+import User from "../models/User.js";
 
 const router = express.Router();
 
+
 router.post("/", async (req, res) => {
-    res.send("user signup")
-})
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        throw new BadRequestError("Please provide all values");
+    };
+
+    const existingUser = User.findOne({ email: email });
+
+    if (existingUser) {
+        throw new BadRequestError("Email is already is use!");
+
+        return;
+    }
+});
 
 router.post("/login", async (req, res) => {
     res.send("user login")
